7eb1042004e5e01fa1684ff70deb5e0aeca8f6b9
begemotoff35 <begemotoff35@yandex.ru>
2020-10-22 20:46:01 +0300

    + admin

--- vacancies/admin.py
+++ vacancies/admin.py
@@ -1,3 +1,21 @@
-# from django.contrib import admin
+from django.contrib import admin
+from .models import Specialty, Company, Vacancy
 
-# Register your models here.
+
+class SpecialtyAdmin(admin.ModelAdmin):
+    fields = ('code', 'title', 'picture')
+    # readonly_fields = ('code', 'title', 'picture')
+    pass
+
+
+class CompanyAdmin(admin.ModelAdmin):
+    pass
+
+
+class VacancyAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Specialty, SpecialtyAdmin)
+admin.site.register(Company, CompanyAdmin)
+admin.site.register(Vacancy, VacancyAdmin)
