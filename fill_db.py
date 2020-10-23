68966a26e30936cb79d33ec2c794f5a23025b9f9
begemotoff35 <begemotoff35@yandex.ru>
2020-10-23 11:03:06 +0300

    + migration

--- fill_db.py
+++ fill_db.py
@@ -14,29 +14,33 @@ from vacancies.models import Vacancy, Company, Specialty
 if __name__ == '__main__':
     print('Please wait...', end='')
 
+    Vacancy.objects.all().delete()
+    Specialty.objects.all().delete()
+    Company.objects.all().delete()
+
     # заполняем специальности:
     for spec in specialties:
-        Specialty.objects.update_or_create(code=spec['code'], title=spec['title'], picture=spec['picture'])
+        Specialty.objects.create(code=spec['code'], title=spec['title'], picture=spec['picture'])
 
     # заполняем компании:
     for company in companies:
-        Company.objects.update_or_create(name=company['title'],
-                                         location=company['location'],
-                                         logo=company['logo'],
-                                         description=company['description'],
-                                         employee_count=company['employee_count'],
-                                         )
+        Company.objects.create(name=company['title'],
+                               location=company['location'],
+                               logo=company['logo'],
+                               description=company['description'],
+                               employee_count=company['employee_count'],
+                               )
 
     # заполняем вакансии:
     for vacancy in jobs:
         specialty = Specialty.objects.filter(code=vacancy['cat']).first()
         company = Company.objects.filter(name=vacancy['company']).first()
-        Vacancy.objects.update_or_create(title=vacancy['title'],
-                                         specialty=specialty,
-                                         company=company,
-                                         description=vacancy['desc'],
-                                         salary_min=vacancy['salary_from'],
-                                         salary_max=vacancy['salary_to'],
-                                         published_at=datetime.strptime(vacancy['posted'], '%Y-%m-%d'),
-                                         )
+        Vacancy.objects.create(title=vacancy['title'],
+                               specialty=specialty,
+                               company=company,
+                               description=vacancy['desc'],
+                               salary_min=vacancy['salary_from'],
+                               salary_max=vacancy['salary_to'],
+                               published_at=datetime.strptime(vacancy['posted'], '%Y-%m-%d'),
+                               )
     print('ok')
