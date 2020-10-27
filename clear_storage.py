ece12184ad3f6e5f63c0c88c199a0d396698cd93
begemotoff35 <begemotoff35@yandex.ru>
2020-10-27 18:14:18 +0300

    + ApplicationAdmin

--- clear_storage.py
+++
@@ -1,5 +0,0 @@
-# Чистим storage
-from django.core.files.storage import get_storage_class
-
-storage = get_storage_class()
-print(storage)
--- vacancies/admin.py
+++ vacancies/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Specialty, Company, Vacancy
+from .models import Specialty, Company, Vacancy, Application
 
 
 class SpecialtyAdmin(admin.ModelAdmin):
@@ -16,6 +16,12 @@ class VacancyAdmin(admin.ModelAdmin):
     pass
 
 
+class ApplicationAdmin(admin.ModelAdmin):
+    list_display = ('written_username', 'written_phone', 'vacancy', 'user')
+    pass
+
+
 admin.site.register(Specialty, SpecialtyAdmin)
 admin.site.register(Company, CompanyAdmin)
 admin.site.register(Vacancy, VacancyAdmin)
+admin.site.register(Application, ApplicationAdmin)
